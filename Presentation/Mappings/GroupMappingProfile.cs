d16434385a0e89110c222f8601425b6bbd4d0a18
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-03-07 09:32:29 -0800

    Add Mappings

---
+++ Presentation/Mappings/GroupMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using VitruviSoft.SamvelAvagyan.Presentation.Models;
+using VitruviSoft.SamvelAvagyan.Repository.Models;
+
+namespace Presentation.Mappings
+{
+    public class GroupMappingProfile : Profile
+    {
+        public GroupMappingProfile()
+        {
+            CreateMap<GroupViewModel, Group>()
+                .ForMember(m => m.Active, p => p.Ignore())
+                .ForMember(m => m.CreatedOn, p => p.Ignore())
+                .ForMember(m => m.ModifiedOn, p => p.Ignore())
+                .ForMember(m => m.Providers, p => p.Ignore());
+        }
+    }
+}
---
+++ Presentation/Mappings/ProviderMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using VitruviSoft.SamvelAvagyan.Presentation.Models;
+using VitruviSoft.SamvelAvagyan.Repository.Models;
+
+namespace Presentation.Mappings
+{
+    public class ProviderMappingProfile : Profile
+    {
+        public ProviderMappingProfile()
+        {
+            CreateMap<ProviderViewModel, Provider>()
+                .ForMember(m => m.Active, p => p.Ignore())
+                .ForMember(m => m.CreatedOn, p => p.Ignore())
+                .ForMember(m => m.ModifiedOn, p => p.Ignore());
+        }
+    }
+}
